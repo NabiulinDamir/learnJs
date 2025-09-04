b0d64271359cd111f7dca60b833ce1bb4f913323
NabiulinDamir <wirelles2015@gmail.com>
2025-09-04 12:56:12 +0500

    saving before deleting the edit button

--- financial_management_system/db.js
+++ financial_management_system/db.js
@@ -7,11 +7,8 @@ const localDB = {
                 if (!db.objectStoreNames.contains('operations')) {
                     db.createObjectStore('operations', { keyPath: 'id', autoIncrement: true });
                 }
-                if (!db.objectStoreNames.contains('expens_categories')) {
-                    db.createObjectStore('expens_categories', { keyPath: 'id', autoIncrement: true });
-                }
-                if (!db.objectStoreNames.contains('income_categories')) {
-                    db.createObjectStore('income_categories', { keyPath: 'id', autoIncrement: true });
+                if (!db.objectStoreNames.contains('categories')) {
+                    db.createObjectStore('categories', { keyPath: 'id', autoIncrement: true });
                 }
             };
 
@@ -22,14 +19,17 @@ const localDB = {
         });
     },
 
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////Основные
+
+    //Добавление
+    //name = "operations" || "categories"
     add: async (name, object) => {
-
         const db = await localDB.open();
-
         let transaction = db.transaction(name, "readwrite");
-        let data = transaction.objectStore(name);
+        let store = transaction.objectStore(name);
         return new Promise((resolve, reject) => {
-            let request = data.add(object);
+            let request = store.add(object);
 
             request.onsuccess = () => {
                 resolve(request.result); // Вернет ID созданного элемента
@@ -45,10 +45,10 @@ const localDB = {
     getAll: async (name) => {
         const db = await localDB.open();
         let transaction = db.transaction(name, "readonly");
-        let data = transaction.objectStore(name);
+        let store = transaction.objectStore(name);
 
         return new Promise((resolve, reject) => {
-            let request = data.getAll();
+            let request = store.getAll();
             request.onsuccess = () => resolve(request.result);
             request.onerror = () => reject(request.error);
         });
@@ -58,20 +58,37 @@ const localDB = {
     get: async (name, id) => {
         const db = await localDB.open();
         let transaction = db.transaction(name, "readonly");
-        let data = transaction.objectStore(name);
+        let store = transaction.objectStore(name);
 
         return new Promise((resolve, reject) => {
-            let request = data.get(id);
+            let request = store.get(id);
             request.onsuccess = () => resolve(request.result);
             request.onerror = () => reject(request.error);
         });
     },
 
-    set: async (id, newObject) => {
-        
+    update: async (name, id, newObject) => {
+        const db = await localDB.open();
+        const transaction = db.transaction(name, "readwrite");
+        const store = transaction.objectStore(name);
+
+        const data = await localDB.get(name, id);
+        if (!data) throw new Error("Объект не найден");
+
+        return await new Promise((resolve, reject) => {
+            const request = store.put({ ...data, ...newObject });
+            request.onsuccess = () => resolve(request.result);
+            request.onerror = () => reject(request.error);
+        });
+    },
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////Вспомогалки
+
+    getCategoryIdForName: async (categoryName) => {
+        const allCategories = await localDB.getAll("categories")
+        return allCategories.find(category => category.name === categoryName).id ?? null
     }
-
-
 };
 
 
--- financial_management_system/main.js
+++ financial_management_system/main.js
@@ -4,62 +4,56 @@ import localDB from './db.js'
 setDataForTable()
 async function setDataForTable() {
     let allOperations = await localDB.getAll("operations")
-    let incomeData = allOperations.filter(op => op.value > 0)
-    let expensData = allOperations.filter(op => op.value < 0)
-    let incomeCategoriesData = await localDB.getAll("income_categories")
-    let expensCategoriesData = await localDB.getAll("expens_categories")
+    let incomeData = allOperations.filter(op => op.type > 0)
+    let expensData = allOperations.filter(op => op.type < 0)
+    let allCategories = await localDB.getAll("categories")
 
-    // localDB.add("operations", {value: 140, date: new Date, categoryId: 1})
-    // localDB.add("operations", {value: 2500, date: new Date, categoryId: 2})
-    // localDB.add("operations", {value: 230, date: new Date, categoryId: 2})
-
-
-    // localDB.add("operations", {value: -140, date: new Date, categoryId: 1})
-    // localDB.add("operations", {value: -1300, date: new Date, categoryId: 2})
-    // localDB.add("operations", {value: -250, date: new Date, categoryId: 3})
-    // localDB.add("operations", {value: -1440, date: new Date, categoryId: 2})
+    // localDB.add("operations", {value: 140, type: 1, date: new Date, categoryId: 1})
+    // localDB.add("operations", {value: 2500, type: 1, date: new Date, categoryId: 2})
+    // localDB.add("operations", {value: 230, type: 1, date: new Date, categoryId: 1})
 
 
+    // localDB.add("operations", {value: 140, type: -1, date: new Date, categoryId: 3})
+    // localDB.add("operations", {value: 1300, type: -1, date: new Date, categoryId: 4})
+    // localDB.add("operations", {value: 250, type: -1, date: new Date, categoryId: 5})
+    // localDB.add("operations", {value: 1440, type: -1, date: new Date, categoryId: 4})
 
+    // localDB.add("categories", {name: "зарплата",  type: 'income' })
+    // localDB.add("categories", {name: "подработка",  type: 'income' })
 
-    // localDB.add("income_categories", {name: "зарплата"})
-    // localDB.add("income_categories", {name: "подработка"})
-
-
-
-    // localDB.add("expens_categories", {name: "еда"})
-    // localDB.add("expens_categories", {name: "топливо"})
-    // localDB.add("expens_categories", {name: "парковка"})
+    // localDB.add("categories", {name: "еда", type: 'expens' })
+    // localDB.add("categories", {name: "топливо", type: 'expens'})
+    // localDB.add("categories", {name: "парковка", type: 'expens'})
 
 
 
-    const fillTable = (htmlElement, dataArray, CategoriesDataArray) => {
+    const fillTable = (htmlElement, dataArray) => {
         // console.log(dataArray)
         const tbody = htmlElement.querySelector('tbody');
         tbody.innerHTML = "";
 
         dataArray.forEach(data => {
             const row = document.createElement('tr');
-
-            const category = CategoriesDataArray.find(category => category.id === data.categoryId);
+            const category = allCategories.find(category => category.id === data.categoryId);
             const date = new Date(data.date);
-
             row.innerHTML = `
             <td>${category?.name || 'Unknown'}</td>
             <td>${data.value}</td>
-            <td>${addNul(date.getDate())}.${addNul(date.getMonth() + 1)}.${addNul(date.getFullYear())}</td>
+            <td>${formatDate(date)}</td>
             <td>
-                <button>···</button>
+                <button class="flat_button">···</button>
             </td>`;
-
-            row.querySelector('button').addEventListener("click", () => { showEditForm(data.id); });
+            
+            row.querySelector('button').addEventListener("click", () => { showEditForm(data); });
+            
+            // row.addEventListener("click", () => {showEditForm(data);})
 
             tbody.appendChild(row);
         });
     }
 
-    fillTable(main_table_income, incomeData, incomeCategoriesData)
-    fillTable(main_table_expens, expensData, expensCategoriesData)
+    fillTable(main_table_income, incomeData)
+    fillTable(main_table_expens, expensData)
 }
 
 
@@ -68,55 +62,58 @@ async function setDataForTable() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////автоматическая работа с форомй
 
 //установка значений в селекторе категорий
-//categoryDataName = 'income_categories' || 'expens_categories'
-async function updateSelectorValues(categoryDataName){
-    let categoryDataArray = await localDB.getAll(categoryDataName)
+//categoryType = 'income' || 'expens'
+async function updateSelectorValues(categoryType) {
+    let categoryAllDataArray = await localDB.getAll('categories')
+    let ategoryDataArray = categoryAllDataArray.filter(category => category.type === categoryType)
     category_select.innerHTML = ''
     category_input.value = ''
-    categoryDataArray.forEach(category => {
+    ategoryDataArray.forEach(category => {
         category_select.innerHTML += `<option value="${category.name}"></option>`;
     });
     return
 }
 
 //установка дефолтных значений формы создания
-function setDefaultValuesForAddForm(){
-    const data = new Date
-    date_selector.value = `${addNul(data.getFullYear())}-${addNul(data.getMonth())}-${addNul(data.getDate())}`
+function setDefaultValuesForAddForm() {
+    date_selector.value = formatDate(new Date)
     value_input.value = ""
     value_input.classList.remove("err")
+    category_input.classList.remove("err")
 }
 
 //установка значений по id операции для формы редактирования
-async function setValuesForEditForm(id){
-    const dataObject = await localDB.get("operations", id) 
+async function setValuesForEditForm(dataObject) {
     value_input.value = dataObject.value
-    const isIncomeCategories = Boolean(dataObject.value > 0) 
-    const categoryDataName = isIncomeCategories ? "income_categories" : "expens_categories"
-    await updateSelectorValues(categoryDataName)
-    const categoryData = await localDB.get(categoryDataName, dataObject.categoryId)
+    await updateSelectorValues(dataObject.type > 0 ? 'income' : 'expens')
+    const categoryData = await localDB.get("categories", dataObject.categoryId)
     category_input.value = categoryData.name
-    date_selector.value =  `${addNul(dataObject.date.getFullYear())}-${addNul(dataObject.date.getMonth())}-${addNul(dataObject.date.getDate())}`
+    date_selector.value = formatDate(dataObject.date)
 }
 
 //проверка валидности формы
-function isFormValid(){
+function isFormValid() {
     value_input.classList.remove("err")
     category_input.classList.remove("err")
-    const isValueValid = (+value_input.value != 0)
+    const isValueValid = (+value_input.value > 0)
     const isSelectorValid = (category_input.value)
-    if(!isValueValid){ value_input.classList.add("err") }
-    if(!isSelectorValid){ category_input.classList.add("err")}
+    if (!isValueValid) { value_input.classList.add("err") }
+    if (!isSelectorValid) { category_input.classList.add("err") }
     return isValueValid && isSelectorValid
 }
 
-
+async function getObjectForm() {
+    const value = +value_input.value
+    const date = new Date(date_selector.value)
+    const categoryId = await localDB.getCategoryIdForName(category_input.value)
+    return { value, date, categoryId }
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////ивенты
 
 //показать окно с добавлением дохода
 table_manager_button_income.addEventListener("click", async () => {
-    await updateSelectorValues("income_categories")
+    await updateSelectorValues("income")
     setDefaultValuesForAddForm()
     save_form_button.addEventListener("click", createIncomeOperation)
     create_form_container.style.visibility = 'visible'
@@ -124,7 +121,7 @@ table_manager_button_income.addEventListener("click", async () => {
 
 //показать окно с добавлением расхода
 table_manager_button_expens.addEventListener("click", async () => {
-    await updateSelectorValues("expens_categories")
+    await updateSelectorValues("expens")
     setDefaultValuesForAddForm()
     save_form_button.addEventListener("click", createExpensOperation)
     create_form_container.style.visibility = 'visible'
@@ -137,32 +134,33 @@ hide_form_button.addEventListener("click", () => {
 })
 
 //показать окно с редактированием дохода
-async function showEditForm(id){
-    await setValuesForEditForm(id)
+async function showEditForm(dataObject) {
+    await setValuesForEditForm(dataObject)
+    save_form_button.addEventListener("click", () => { editOperation(dataObject.id) })
     create_form_container.style.visibility = 'visible'
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////создание операций
 
 //ивент создания дохода
-async function createIncomeOperation(){
-    if(!isFormValid())return
+async function createIncomeOperation() {
+    if (!isFormValid()) return
     const value = +value_input.value
-    const categoryId = await findIncomeCategory(category_input.value)
-    createOperation(value, categoryId, new Date(date_selector.value))
+    const categoryId = await findCategory(category_input.value, 'income')
+    createOperation(value, categoryId, new Date(date_selector.value), 1)
 }
 
 //ивент создания расхода
-async function createExpensOperation(){
-    if(!isFormValid())return
-    const value = +value_input.value * -1
-    const categoryId = await findExpensCategory(category_input.value)
-    createOperation(value, categoryId, new Date(date_selector.value))
+async function createExpensOperation() {
+    if (!isFormValid()) return
+    const value = +value_input.value
+    const categoryId = await findCategory(category_input.value, 'expens')
+    createOperation(value, categoryId, new Date(date_selector.value), -1)
 }
 
 //создание операции
-async function createOperation(value, categoryId, date) {
-    await localDB.add("operations", { value, date, categoryId });
+async function createOperation(value, categoryId, date, type) {
+    await localDB.add("operations", { value, date, categoryId, type });
     setDataForTable()
     clearEventsForSaveButton()
     create_form_container.style.visibility = 'hidden'
@@ -170,42 +168,37 @@ async function createOperation(value, categoryId, date) {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////редактирование операций
 
-//редактирование дохода
-async function editIncomeOperation(params) {
-    
+//редактирование операции(расхода или дохода)
+async function editOperation(operationId) {
+    if (!isFormValid()) return
+    const newObj = await getObjectForm()
+    console.log("operations", operationId, newObj)
+    create_form_container.style.visibility = 'hidden'
+    clearEventsForSaveButton()
 }
 
-//редактирование расхода
-async function editExpensOperation(params) {
-    
-}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////создание категорий
 
-//если нету категории дохода, то создаёт
-async function findIncomeCategory(name) {
-    const incomeCategories = await localDB.getAll("income_categories")
-    let   findCategoryId = incomeCategories.find(category => category.name === name)?.id
-    if(!findCategoryId){
-        findCategoryId = await localDB.add('income_categories', { name })
+//если нету категории, то создаёт
+//type === 'income' || 'expens'
+async function findCategory(name, type) {
+    const allCategories = await localDB.getAll("categories")
+    const categories = allCategories.filter(category => category.type === type)
+    let findCategoryId = categories.find(category => category.name === name)?.id
+    if (!findCategoryId) {
+        findCategoryId = await localDB.add("categories", { name, type })
     }
     return findCategoryId
 }
 
-//если нету категории расхода, то создаёт
-async function findExpensCategory(name) {
-    const incomeCategories = await localDB.getAll("expens_categories")
-    let   findCategoryId = incomeCategories.find(category => category.name === name)?.id
-    if(!findCategoryId){
-        findCategoryId = await localDB.add('expens_categories', { name })
-    }
-    return findCategoryId
-}
 
 //сброс ивента
-function clearEventsForSaveButton(){
+function clearEventsForSaveButton() {
     save_form_button.removeEventListener("click", createIncomeOperation)
     save_form_button.removeEventListener("click", createExpensOperation)
+    save_form_button.removeEventListener("click", () => {editOperation()})
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -215,4 +208,14 @@ function btnPress(id) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const addNul = (number) => number < 9 ? `0${number}` : number
+
+const formatDate = (dateObj) => {
+    const addNul = (number) => number < 9 ? `0${number}` : number
+    return `${addNul(dateObj.getFullYear())}-${addNul(dateObj.getMonth())}-${addNul(dateObj.getDate())}`
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////Клики по линиям
+
+function clickToTr(data){
+    console.log("trClick: " + data.id )
+}
